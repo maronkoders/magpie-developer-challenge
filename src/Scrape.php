9052d455fed88d8dcaa98ad31fa785cb321b2cba
INNOCENTGWARA <inngwar664@gmail.com>
2022-12-12 17:42:39 +0200

    modified to encode output to jsn file

--- src/Scrape.php
+++ src/Scrape.php
@@ -1,67 +1,29 @@
 <?php
 
-namespace App;
-
-require 'vendor/autoload.php';
-
+require 'vendor/autoload.php'; 
 
 use Symfony\Component\DomCrawler\Crawler;
 
-class Scrape
-{
-    use Utils;
-    
-    private array $smartphoness = [];
-    private const TOTAL_PAGES = 3;
+$url = "https://www.magpiehq.com/developer-challenge/smartphones/?page=1";
+$html = file_get_contents($url); 
+
+$crawler = new Crawler($html);
+$item = [];
+// loop through the values
+
+$nodeValues = $crawler->filter('div.mb-12 > div.rounded-md')->each(function (Crawler $node, $i) {
+    //searching for the desired values
    
-    public function run(): void
-    {  
-        for($i=1;  $i <= SELF::TOTAL_PAGES ; $i++)
-        {
-            $document = ScrapeHelper::fetchDocument($this->baseUrl.'/smartphones/?page='.$i);
-            $document->filter('div.-mx-4 > div.mb-12')->each(function (Crawler $node)
-            {
-                try {
-                  $node->filter('div.mb-12 > div.rounded-md')->each(function (Crawler $product)
-                        {
-                            $product->filter('div.my-4 > div.-mx-2 > div.px-2')->each(function (Crawler $color) use ($product) 
-                                {   
-                                        $productObj = new Product();
-                                        $this->smartphones[]  = $productObj->properties($product, $color);
-                                });    
-                        });
-                  
-                } catch (\InvalidArgumentException $e) {
-                    echo 'failed to scrape smartphone data data ' . $e->getMessage() . $e->getMessage();
-                }
-            });
-        }
 
-        $result = $this->dedup($this->smartphones);
+// storing values in any array
+$text = $node->text();
+$image = $node->filter('img')->attr('src');
+$item = [$image, $text];
 
-        file_put_contents('output.json', json_encode($result));
-    }
+//echo($item);
 
-    public  function searchDuplicate($arr, $obj) {
-        foreach ($arr as $value) {
-            if ($value['title'] == $obj['title'] && $value['color'] == $obj['color'] && $value['price'] == $obj['price']) {
-                return true;
-            }
-        }
-        return false;
-    }
+file_put_contents('output.json', json_encode($item));
 
-    public function dedup($arr)
-    {
-        $result = array();
-        foreach ($arr as $obj) {
-            if ($this->searchDuplicate($result, $obj) === false) {
-                $result[] = $obj;
-            }
-        }
-        return $result;
-    }
-}
+});
 
-$scrape = new Scrape();
-$scrape->run();
+
